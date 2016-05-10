4fa0955a35f0d651a1b2da3a1e2698e5bebd150e
Jose Flavio A dot <joseflavio-at-gmail-dot-com>
2016-05-10 17:00:40 +0200

    Small fixes

--- prepare_dictionary.cmd
+++ prepare_dictionary.cmd
@@ -1,2 +1,9 @@
+where /q uniq
+IF ERRORLEVEL 1 (
+    ECHO The application "uniq" is missing, maybe Cygwin is not installed?
+    ECHO Ensure it is installed and placed in your PATH.
+    EXIT /B
+)
+
 copy eclipse-java-dictionary.txt eclipse-java-dictionary.bak
 sort eclipse-java-dictionary.bak | uniq > eclipse-java-dictionary.txt
